5cba3b3fff8cab82e0a8285f2deaf5bd96376c83
undersky0 <richardlonesteen@gmail.com>
2014-11-05 08:15:59 +0000

    secret

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uu2::Application.config.secret_token = '02ddeda2649c7bd97c90700a18a2c828256d7e3be55a87869ea7573489dde65cf720f1978dc044ac4439b172d29193b591362a50e5643bee7a33e6e33b92531d'
+Uu2::Application.config.secret_key_base = '02ddeda2649c7bd97c90700a18a2c828256d7e3be55a87869ea7573489dde65cf720f1978dc044ac4439b172d29193b591362a50e5643bee7a33e6e33b92531d'
