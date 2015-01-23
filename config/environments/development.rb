07fa36a5aa80dda371d6cef19ceb14bb3728e753
undersky0 <richardlonesteen@gmail.com>
2015-01-23 00:41:01 +0000

    home update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   port:                 587,
   domain:               'gmail.com',
   user_name:            'richardlonesteen@gmail.com',
-  password:             'piratas12',
+  password:             'pass',
   authentication:       'plain',
   enable_starttls_auto: true  }
   
